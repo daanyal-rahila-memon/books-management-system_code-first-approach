87557c69bee01071c45f2fe9c78e212102759a2e
Muhammad Daanyal <daanyalhere@gmail.com>
2023-07-09 21:22:26 +0500

    Added Book Module & it's resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { BookModule } from './book/book.module';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      playground: true, // never be done 'true' in production
+      autoSchemaFile: join(process.cwd(), "src/schema.graphql")
+    }),
+    BookModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
